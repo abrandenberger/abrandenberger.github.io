6d41df711f119860eb55a23076549a5de329625d
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2021-02-13 03:08:44 -0500

    big overhaul

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function togglein() {
 }
 
 function insertCss(code) {
-    var style = document.createElement('style');
+    let style = document.createElement('style');
     style.type = 'text/css';
 
     if (style.styleSheet) {
@@ -36,15 +36,52 @@ function insertCss(code) {
     document.getElementsByTagName("head")[0].appendChild(style);
 }
 
-let scrollCSS = "::-webkit-scrollbar { width: .8em;}" 
-+ "::-webkit-scrollbar-track {background: lightgray; border: .35em solid #f0f0f0; border-radius: 10px; }" 
-+ "::-webkit-scrollbar-track { background: lightgray; border: .35em solid #f0f0f0; border-radius: 10px; }" 
-+ "::-webkit-scrollbar-thumb { background: #f0b1b1; border: .3em solid #f0f0f0; }" 
-+ "::-webkit-scrollbar-thumb:hover { background: #e05c5c; }"
+let scrollCSS = "::-webkit-scrollbar { width: .8em;}"
+    + "::-webkit-scrollbar-track {background: lightgray; border: .35em solid #f0f0f0; border-radius: 10px; }"
+    + "::-webkit-scrollbar-track { background: lightgray; border: .35em solid #f0f0f0; border-radius: 10px; }"
+    + "::-webkit-scrollbar-thumb { background: #f0b1b1; border: .3em solid #f0f0f0; }"
+    + "::-webkit-scrollbar-thumb:hover { background: #e05c5c; }"
 
+function toggleHeight(clicked) {
+    if (clicked.style.maxHeight) {
+        clicked.style.maxHeight = null;
+    } else {
+        clicked.style.maxHeight = clicked.scrollHeight + "px";
+    }
+
+}
 window.addEventListener('load', () => {
     if (window.navigator.platform != 'MacIntel') {
         insertCss(scrollCSS);
-        console.log('hi!');
     }
+
+    let coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            let contentList = document.getElementsByClassName("collapsible-content");
+            let clicked = contentList[i];
+            let actives = [];
+            for (let j = 0; j < coll.length; j++) {
+                if (coll[j].classList.contains("active")) {
+                    actives.push(j);
+                }
+            }
+            if (actives.length == 0 || actives[0] == i) {
+                // none are open or the clicked one is open
+                toggleHeight(clicked);
+                coll[i].classList.toggle("active");
+            }
+            else {
+                // one is open but not the one clicked now 
+                toggleHeight(contentList[actives[0]]);
+                coll[actives[0]].classList.toggle("active");
+                setTimeout(() => {
+                    toggleHeight(clicked);
+                    coll[i].classList.toggle("active");
+                    }, 250);
+            }
+        });
+    }
+    // start with one open
+    // coll[0].click();
 })
