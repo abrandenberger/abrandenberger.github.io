34660df67369cd09bedc91a69df5aea88f5a51da
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2024-12-08 06:16:04 -0500

    new vers done

--- telescoping-text.js
+++ telescoping-text.js
@@ -1,18 +1,20 @@
 const enjoyContent = `
-* Outside of scribbling mathematical nonsense, an unordered collection of things I enjoy: 
-* building playful communal software like this <a href="https://curius.app/">social bookmarker</a>, 
+* Outside of scribbling mathematical nonsense, I run <a href="https://math.mit.edu/arts/">Friends of the Arts</a>
+* at MIT, build fun communal software (check out <a href="https://curius.app/">Curius</a>, a social bookmarker), 
+* and enjoy (as an unordered collection)
 * creating <a href="https://open.spotify.com/playlist/0mgJc653Q2kYsSQEwfK7AT?si=C1_8FKzLSumn8wY-7F5C7g">oddly</a> 
 * <a href="https://open.spotify.com/playlist/3H1nr18GB6LPB6myTYek7c?si=IO34RAyIRjGXt_X_bGC5rQ">specific</a> 
 * <a href="https://open.spotify.com/playlist/2IudEQhWMGZI6ucYxl4rbD?si=zEw3BPzPRMu6q1imp911bw">playlists</a>,
 * playing piano, 
   * playing piano and cursing Rachmaninov's handspan, 
-    * receiving recommendations, 
-      * listening to friends ramble, 
-        * catching a perfect photo, 
-          * seeing things i created being used in the wild, 
+  * receiving recommendations, 
+    * listening to friends ramble, 
+      * catching a perfect photo, 
+        * seeing things i created being used in the wild, 
+          * mixing chemicals in the darkroom,
             * committing to the bit, 
               * marveling, 
-                * <a href="https://www.clarkart.edu/">well</a>-<a href="https://massmoca.org/">curated</a> <a href="https://www.fotografiska.com/">art</a> <a href="https://louisiana.dk/en/">museums</a>,
+                * <a href="https://www.clarkart.edu/">well</a>-<a href="https://massmoca.org/">curated</a> <a href="https://stockholm.fotografiska.com/">art</a> <a href="https://louisiana.dk/en/">museums</a>,
                 * flashes of insight, 
                     * the hush before an orchestra begins to play, 
                       * <a href="https://www.sonetseigle.com/">buttery</a> <a href="https://aukouingamann.com/">croissants</a>,
@@ -22,15 +24,14 @@ const enjoyContent = `
                           * long walks,
                             * libraries,
                               * lists,
-                                * airports,
-                                  * bass lines,
-                                    * concert crowds,
-                                      * weird calendar invites,
-                                        * delightful interactions,
-                                          * warm lighting,
-                                            * working public transit,
-                                              * yuzu-flavoured anything,
-                                                * the plateau,
+                                * bass lines,
+                                  * concert crowds,
+                                    * weird calendar invites,
+                                      * delightful interactions,
+                                        * warm lighting,
+                                          * working public transit,
+                                            * yuzu-flavoured anything,
+                                              * the plateau,
 * and more.`;
 const enjoyConfig = { textMode: TextMode.Html };
 const enjoyNode = createTelescopicTextFromBulletedList(
